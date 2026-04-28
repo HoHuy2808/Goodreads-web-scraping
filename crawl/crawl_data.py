b6d2597eeb0c9cd49ea9a33227b1e56d8cb30a6a
HoHuy2808 <hohuy.work@gmail.com>
2026-04-28 23:43:43 +0700

    add number of genres

---
+++ crawl/crawl_data.py
@@ -0,0 +1,73 @@
+import os
+import requests
+import json
+
+from bs4 import BeautifulSoup
+from tqdm import tqdm
+from operate import get_book
+
+headers = {
+    'User-Agent': "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/145.0.0.0 Safari/537.36",
+    'Accept-language': 'US-en'}
+
+def get_data(num_genres=40, max_page=20):
+
+    book_data = []
+    bookList = get_book(num_genres=num_genres, max_page=max_page)
+
+    for book in tqdm(bookList):
+
+        # book_url = bookList[idx]
+        book_url = book
+        response = requests.get(book_url, headers=headers) 
+        soup = BeautifulSoup(response.text, "html.parser")
+        div = soup.find("div",{"class":"BookPage__mainContent"})
+
+        name = book_url.split("/book/show/")[-1]
+        print(f"Scraping book {name}")
+
+        if div is None:
+            print(f"Skipping {book_url} — book not found")
+            continue
+
+        # Title
+        book_title = div.find("h1",{"class":"Text Text__title1"}).text.replace("'"," ").replace(":","")
+
+        # Author
+        book_author = div.find("span",{"class":"ContributorLink__name"}).text
+
+        # Ratings
+        book_rating = div.find("div", {"class":"RatingStatistics__rating"}).text
+
+        # Genres
+        sub_div = soup.find("ul",{"class":"CollapsableList"})
+        if sub_div is None:
+            print(f"Genres not found")
+            book_genres = 'null'
+        else:
+            raw_genres = sub_div.find_all("span",{"class":"Button__labelItem"})
+            genres = [genre.get_text(strip=True) for genre in raw_genres if genre.get_text(strip=True)!="...more"]
+            book_genres = ", ".join(genres)
+
+        # Details
+        book_detail = div.find("span",{"class":"Formatted"}).text
+
+        dict = {'Title': book_title,
+                    'Author': book_author,
+                    'Ratings': book_rating,
+                    'Genres': book_genres,
+                    'Detail': book_detail}
+        book_data.append(dict)
+
+
+    return book_data
+
+if __name__ == "__main__":
+    data = get_data(num_genres=2, max_page=2)
+
+    os.makedirs("data", exist_ok=True)
+
+    with open("data/book_data.json", "w", encoding="utf-8") as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+
+    print("Saved data")
---
+++ crawl/operate.py
@@ -0,0 +1,113 @@
+import os
+import time
+import requests
+import get_variables as gvar
+from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+headers = {
+    'User-Agent': gvar.user_agent,
+    'Accept-language': 'US-en'}
+
+# load env
+load_dotenv()
+EMAIL = os.getenv("GOODREADS_EMAIL")
+PASSWORD = os.getenv("GOODREADS_PASSWORD")
+
+# Crawl genres
+def get_genres():
+    genres_url = f"{gvar.goodreads}/genres"
+    response = requests.get(genres_url, headers=headers) 
+    soup = BeautifulSoup(response.text, "html.parser")
+    container = soup.find_all('div', {'class': 'left'})
+
+    genres = []
+    for div in container:
+        links = div.find_all('a', class_='gr-hyperlink')
+        for link in links:
+
+            name = link.text.lower().replace(" ","-").replace("'","")
+            genres.append(name)
+
+    return genres
+
+
+# Crawl book url
+def get_genres():
+    genres_url = f"{gvar.goodreads}/genres"
+    response = requests.get(genres_url, headers=headers) 
+    soup = BeautifulSoup(response.text, "html.parser")
+    container = soup.find_all('div', {'class': 'left'})
+
+    genres = []
+    for div in container:
+        links = div.find_all('a', class_='gr-hyperlink')
+        for link in links:
+
+            name = link.text.lower().replace(" ","-").replace("'","")
+            genres.append(name)
+
+    return genres
+
+
+# Crawl
+def get_book(num_genres=40, max_page=20):
+
+    book_list = []
+    genres = get_genres()[:num_genres]
+
+    chrome_options = Options()
+    chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+
+    driver = webdriver.Chrome(options=chrome_options)
+    wait = WebDriverWait(driver, 10)
+
+    # Login
+    driver.get(f"{gvar.goodreads}/user/sign_in")
+
+    time.sleep(10)
+
+    if "sign_in" in driver.current_url:
+        driver.find_element(By.LINK_TEXT, "Sign in with email").click()
+
+        wait.until(EC.presence_of_element_located((By.ID, "ap_email")))
+
+        driver.find_element(By.ID, "ap_email").send_keys(EMAIL)
+        time.sleep(2)
+
+        driver.find_element(By.ID, "ap_password").send_keys(PASSWORD)
+        time.sleep(2)
+        
+        driver.find_element(By.ID, "signInSubmit").click()
+        time.sleep(5)
+        
+        print("Logged in successfully!")
+    else:
+        print("Already logged in")
+
+        
+    for genre in genres:
+        print(f"\n Crawling genre: {genre}")
+
+        for page in range(1, max_page + 1):
+            url = f"{gvar.goodreads}/shelf/show/{genre}?page={page}"
+            driver.get(url)
+
+            wait.until(EC.presence_of_element_located((By.CLASS_NAME, "bookTitle")))
+
+            books = driver.find_elements(By.CLASS_NAME, "bookTitle")
+
+            for book in books:
+                href = book.get_attribute("href")
+                if href:
+                    book_list.append(href)
+
+            print(f"Page {page}: {len(books)} books")
+
+    return book_list
